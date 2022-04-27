1bebf5d8f82d5703b283c593a65d8c8c3efcd2d7
Yanant27 <htooyanant.hyk@gmail.com>
2022-04-27 11:44:53 +0630

    Add in memory authentication using UserDetails.

--- src/main/java/hyk/springframework/springtravellogrestapi/config/WebSecurityConfig.java
+++ src/main/java/hyk/springframework/springtravellogrestapi/config/WebSecurityConfig.java
@@ -1,10 +1,16 @@
 package hyk.springframework.springtravellogrestapi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -12,16 +18,42 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .authorizeRequests(authorize -> {
+                    // permit all request with "GET" for specified ant pattern
+                    authorize.antMatchers(HttpMethod.GET, "/api/v1/travellogs").permitAll()
+                    // permit all request with "GET" for specified mvc pattern
+                    .mvcMatchers(HttpMethod.GET, "/api/v1/travellogs/{id}")
+                            .hasAnyAuthority("ADMIN", "USER")
+//                            .hasAnyRole("ADMIN", "USER")
+                    .antMatchers(HttpMethod.POST).hasAuthority("ADMIN")
+                    .antMatchers(HttpMethod.DELETE).hasAuthority("ADMIN")
+                    .antMatchers(HttpMethod.PUT).hasAuthority("ADMIN");
+                })
                 .authorizeRequests()
-                // permit all request with "GET" for specified ant pattern
-//                .antMatchers(HttpMethod.GET, "/api/v1/travellogs/**").permitAll()
-                // permit all request with "GET" for specified mvc pattern
-                .mvcMatchers(HttpMethod.GET, "/api/v1/travellogs/{id}").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic()
                 .and()
                 .csrf().disable(); // csrf is default in spring
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("hyk")
+                .password("yanant")
+//                .roles("ADMIN")
+                .authorities("ADMIN")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("guest")
+                .password("guest")
+//                .roles("USER")
+                .authorities("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
